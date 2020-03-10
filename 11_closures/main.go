f302858a051f99f2ba82cbab75fb99c2e0d6c865
Luisa Barbalho <luisa.barbalhops@gmail.com>
2020-03-10 00:06:33 -0300

    closures

---
+++ 11_closures/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/*
+Funções anonimas em linha
+*/
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	sum := adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(sum(i))
+	}
+}
